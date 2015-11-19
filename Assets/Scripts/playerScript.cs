5ec91da570165a1a864f062d0a1fe06ecbc1d21a
julien calvo <j.calvo@rubika-edu.com>
2015-11-19 16:05:00 +0100

    ajout joueur

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+    public float speed = 10f;
+    public float deadZone = 0.5f;
+    public int playerNumber;
+    public float gravity = 20.0F;
+
+    bool peutPoser = true;
+
+    public GameObject prefabBombe;
+    public CharacterController cc;
+    Vector3 tr;
+    Vector3 moveDirection;
+
+	// Use this for initialization
+	void Start () {
+        cc = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+       /* //move left
+        if (Input.GetAxis("L_XAxis_" + playerNumber) < -deadZone)
+        {
+            //transform.Translate(-speed, 0f, 0f);
+            cc.SimpleMove(new Vector3(-speed * Time.deltaTime, 0f, 0f));
+        }
+       
+        //move right
+     if (Input.GetAxis("L_XAxis_" + playerNumber) > deadZone)
+          {
+        // transform.Translate(speed, 0f, 0f);
+         cc.SimpleMove(new Vector3(speed * Time.deltaTime, 0f, 0f));
+         }*/
+       
+
+        //move up move up, rough comme une louve
+       //if (Input.GetAxis("L_YAxis_" + playerNumber) > deadZone)
+            //{
+              //  Debug.Log("up");
+          // transform.Translate(0f, 0f, -speed);
+                //cc.SimpleMove(new Vector3(0f, 0f, -speed * Time.deltaTime));
+        
+        if (cc.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("L_XAxis_" + playerNumber), 0, -Input.GetAxis("L_YAxis_" + playerNumber));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        cc.Move(moveDirection * Time.deltaTime);
+         //  }
+     
+
+         //move down
+      // if (Input.GetAxis("L_YAxis_" + playerNumber) < -deadZone)
+          //  {
+              //  Debug.Log("down");
+                /*Vector3 forward = transform.TransformDirection(Vector3.forward);
+                float curSpeed = speed * Input.GetAxis("Vertical");
+                cc.SimpleMove(-forward * curSpeed);*/
+          //  }
+
+       /*if (Input.GetButton("Attack_" + playerNumber))
+       {
+           
+       }*/
+       tr = this.gameObject.transform.position;
+       if (Input.GetButtonDown("Attack_" + playerNumber) && peutPoser)
+       {
+           GameObject bombe = Instantiate(prefabBombe, transform.position, Quaternion.identity) as GameObject;
+           bombe.GetComponent<Bombes>().SetParent(gameObject);
+           peutPoser = false;
+       }
+           
+
+	    
+	}
+
+    public void SetPeutPoser(bool newBool)
+    {
+        peutPoser = newBool;   
+    
+    }
+
+}
