a3eaacdba6f355d50d2479bef44749fa8a22d619
antoinusitos <a.gargasson@rubika-edu.com>
2016-01-07 20:49:23 +0100

    modif de bugs sur le joueur mort

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -22,29 +22,7 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () 
-    {
-       /* //move left
-        if (Input.GetAxis("L_XAxis_" + playerNumber) < -deadZone)
-        {
-            //transform.Translate(-speed, 0f, 0f);
-            cc.SimpleMove(new Vector3(-speed * Time.deltaTime, 0f, 0f));
-        }
-       
-        //move right
-     if (Input.GetAxis("L_XAxis_" + playerNumber) > deadZone)
-          {
-        // transform.Translate(speed, 0f, 0f);
-         cc.SimpleMove(new Vector3(speed * Time.deltaTime, 0f, 0f));
-         }*/
-       
-
-        //move up move up, rough comme une louve
-       //if (Input.GetAxis("L_YAxis_" + playerNumber) > deadZone)
-            //{
-              //  Debug.Log("up");
-          // transform.Translate(0f, 0f, -speed);
-                //cc.SimpleMove(new Vector3(0f, 0f, -speed * Time.deltaTime));
-        
+    {        
         if (cc.isGrounded)
         {
             moveDirection = new Vector3(Input.GetAxis("L_XAxis_" + playerNumber), 0, -Input.GetAxis("L_YAxis_" + playerNumber));
@@ -54,22 +32,7 @@ public class PlayerScript : MonoBehaviour {
         }
         moveDirection.y -= gravity * Time.deltaTime;
         cc.Move(moveDirection * Time.deltaTime);
-         //  }
-     
 
-         //move down
-      // if (Input.GetAxis("L_YAxis_" + playerNumber) < -deadZone)
-          //  {
-              //  Debug.Log("down");
-                /*Vector3 forward = transform.TransformDirection(Vector3.forward);
-                float curSpeed = speed * Input.GetAxis("Vertical");
-                cc.SimpleMove(-forward * curSpeed);*/
-          //  }
-
-       /*if (Input.GetButton("Attack_" + playerNumber))
-       {
-           
-       }*/
        if (Input.GetButtonDown("Attack_" + playerNumber) && peutPoser)
        {
            Vector3 bombPosition = new Vector3(TileMapGenerator.instance.tileMap[TileMapGenerator.instance.CoordToIndex(transform.position.x, transform.position.z)].x, transform.position.y, TileMapGenerator.instance.tileMap[TileMapGenerator.instance.CoordToIndex(transform.position.x, transform.position.z)].z);
@@ -77,15 +40,11 @@ public class PlayerScript : MonoBehaviour {
            bombe.GetComponent<Bombes>().SetParent(gameObject);
            peutPoser = false;
        }
-           
-
-	    
 	}
 
     public void SetPeutPoser(bool newBool)
     {
         peutPoser = newBool;   
-    
     }
 
     public void TakeDamage(int theDamage)
